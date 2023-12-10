c942ab49c1737c528e50eccd6a47bdcc3b7368ce
cal-brynestad <cabr8942@colorado.edu>
2023-12-09 19:29:59 -0700

    3.19-3.21

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -14,10 +14,22 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   })
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
+  const personSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      minLength: 3
+    },
+    number: {
+      type: String,
+      minLength: 8,
+      validate: {
+        validator: function(v) {
+          return /^\d{2,3}-\d+$/.test(v)
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      }
+    }
+  })
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
